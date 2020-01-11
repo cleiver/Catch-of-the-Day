2f6afb41d16d36a5aaab27b652bb8d3afc42ed38
cleiver <cleiver@gmail.com>
2020-01-11 18:04:44 -0300

    Beginning to understand events

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  goToStore = event => {
+    event.preventDefault();
+  };
   render() {
     return (
       <React.Fragment>
         {/* this is how we write comments in react */}
         {/* we cannot return siblings tags, we must wrap it in a parent 😒 */}
 
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please enter a store</h2>
           <input
             type="text"
