dc055e869c85db9e723eacc0fad80f10356a2258
cleiver <cleiver@gmail.com>
2020-01-11 17:47:18 -0300

    Routing

---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div className="order-title">
+    <h1>Store not found</h1>
+    <img src="images/travolta.gif"></img>
+  </div>
+);
+
+export default NotFound;
