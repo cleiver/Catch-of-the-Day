b8a786f8c5f1d60a0d4645f5f8256ab3244be301
cleiver <cleiver@gmail.com>
2020-01-11 21:03:13 -0300

    Playing with states

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,26 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    // We cannot update the state directly, so...
+
+    // 1. get a copy of what we want
+    const fishes = { ...this.state.fishes };
+
+    // update with the new one
+    fishes[`fish${Date.now()}`] = fish;
+
+    // and update the state with the new data
+    this.setState({
+      fishes: fishes
+    });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +31,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"></Header>
         </div>
         <Order></Order>
-        <Inventory></Inventory>
+        <Inventory addFish={this.addFish}></Inventory>
       </div>
     );
   }
